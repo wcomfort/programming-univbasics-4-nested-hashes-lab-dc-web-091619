55d6ce00fe7dd040ee511bfb614cb0cd42dfe65e
Will Comfort <comfortwb21@gmail.com>
2019-09-03 21:35:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] += "a"
+programmer_hash[:dennis_ritchie][:languages] += "Assembly"
+return programmer_hash
 end
