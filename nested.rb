8735a2ae67fa97c7ab74c13b4e1f496d8b8f43d0
Will Comfort <comfortwb21@gmail.com>
2019-09-03 21:36:15 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -81,7 +81,8 @@ def adding_matz
         }
      }
      programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-end
+     return programmer_hash
+     end
 
 
 def changing_alan
@@ -103,7 +104,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -122,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+return programmer_hash
 end
